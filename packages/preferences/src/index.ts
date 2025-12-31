27fb477b0e97cc905237c8c3f56f0f27d5db065e
ppxb <317842449@qq.com>
2026-01-01 00:33:09 +0800

    chore: change preference description

--- packages/preferences/src/index.ts
+++ packages/preferences/src/index.ts
@@ -2,12 +2,10 @@ import type { Preferences } from '@vben-core/preferences';
 import type { DeepPartial } from '@vben-core/typings';
 
 /**
- * 如果你想所有的app都使用相同的默认偏好设置，你可以在这里定义
+ * 如果你想所有的 app 都使用相同的默认偏好设置，你可以在这里定义
  * 而不是去修改 @vben-core/preferences 中的默认偏好设置
- * @param preferences
- * @returns
+ * @param preferences - 偏好设置
  */
-
 function defineOverridesPreferences(preferences: DeepPartial<Preferences>) {
   return preferences;
 }
